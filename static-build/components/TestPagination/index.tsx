ed412e68d63174a09c4f86652d05ac8f58136678
deepanshu <deepanshuyadav.04@gmail.com>
2022-01-06 21:50:41 +0530

    bug

--- static-build/components/TestPagination/index.tsx
+++ static-build/components/TestPagination/index.tsx
@@ -13,54 +13,56 @@
 
 import { h, FunctionalComponent } from 'preact';
 import {
-  $lighter,
-  $linkData1,
-  $linkData2,
-  $nav,
-  $svgLeft,
-  $svgRight,
-  $container,
+    $lighter,
+    $linkData1,
+    $linkData2,
+    $nav,
+    $svgLeft,
+    $svgRight,
+    $svgHolder,
+    $container,
 } from './styles.css';
 interface Props {
-  prev: PaginationData;
-  next: PaginationData;
+    prev: PaginationData;
+    next: PaginationData;
 }
 const TestPagination: FunctionalComponent<Props> = ({ prev, next }: Props) => {
-  return (
-    <div class={$nav}>
-      <div>
-        {' '}
-        <a href={prev.link}>
-          <ul>
-            <li>
-              <svg class={$svgLeft} viewBox="0 0 9 5">
-                <path d="M4.6 2.7L9 0v2.1L4.7 5.3h-.2L0 2.1V0z" />
-              </svg>
-            </li>
-            <li class={$linkData1}>
-              <div>{prev.meta.title}</div>
-              <div class={$lighter}>{prev.meta.shortDesc}</div>
-            </li>
-          </ul>
-        </a>
-      </div>
-      <div>
-        <a href={next.link}>
-          <div class={$container}>
-            <div class={$linkData2}>
-              <div>{next.meta.title}</div>
-              <div class={$lighter}>{next.meta.shortDesc}</div>
+    return (
+        <div class={$nav}>
+            <div>
+                {' '}
+                <a href={prev.link}>
+                    <div class={$container}>
+                        <div class={$svgHolder}>
+                            <svg class={$svgLeft} viewBox="0 0 9 5">
+                                <path d="M4.6 2.7L9 0v2.1L4.7 5.3h-.2L0 2.1V0z" />
+                            </svg>
+                        </div>
+                        <div class={$linkData1}>
+                            <div>{prev.meta.title}</div>
+                            <div class={$lighter}>{prev.meta.shortDesc}</div>
+                        </div>
+                    </div>
+                </a>
             </div>
             <div>
-              <svg viewBox="0 0 9 5">
-                <path d="M4.6 2.7L9 0v2.1L4.7 5.3h-.2L0 2.1V0z" />
-              </svg>
+                <a href={next.link}>
+                    <div class={$container}>
+                        <div class={$linkData2}>
+                            <div>{next.meta.title}</div>
+                            <div class={$lighter}>{next.meta.shortDesc}</div>
+                        </div>
+                        <div class={$svgHolder}>
+                            <svg class={$svgRight}viewBox="0 0 9 5">
+                                <path d="M4.6 2.7L9 0v2.1L4.7 5.3h-.2L0 2.1V0z" />
+                            </svg>
+                        </div>
+                    </div>
+                </a>
             </div>
-          </div>
-        </a>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default TestPagination;
+
